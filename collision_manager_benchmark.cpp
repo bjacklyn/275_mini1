151e876d4799fbbeb1430bed35b18bafbb35ca24
Brandon Jacklyn <brandonjacklyn@live.ca>
2025-02-12 13:22:34 -0800

    Add google benchmark to cmake and add few benchmarks of searching

---
+++ collision_manager_benchmark.cpp
@@ -0,0 +1,61 @@
+#include "collision_manager.hpp"
+
+#include <benchmark/benchmark.h>
+#include <limits>
+
+static std::unique_ptr<CollisionManager> collision_manager;
+
+class CollisionManagerBenchmark : public benchmark::Fixture
+{
+public:
+
+    void SetUp(const ::benchmark::State& state)
+    {
+        if (collision_manager.get() == nullptr) {
+            collision_manager = std::make_unique<CollisionManager>(std::string("../Motor_Vehicle_Collisions_-_Crashes_20250123.csv"));
+        }
+    }
+};
+
+BENCHMARK_DEFINE_F(CollisionManagerBenchmark, SearchSingleStringFieldNoMatches)(benchmark::State& state) {
+    Query query = Query::create("borough", QueryType::EQUALS, "Nothing should match me");
+
+    for (auto _ : state) {
+        std::vector<const Collision*> results = collision_manager->search(query);
+        benchmark::DoNotOptimize(results);
+    }
+}
+
+BENCHMARK_DEFINE_F(CollisionManagerBenchmark, SearchSingleStringFieldSomeMatches)(benchmark::State& state) {
+    Query query = Query::create("borough", QueryType::EQUALS, "BROOKLYN");
+
+    for (auto _ : state) {
+        std::vector<const Collision*> results = collision_manager->search(query);
+        benchmark::DoNotOptimize(results);
+    }
+}
+
+BENCHMARK_DEFINE_F(CollisionManagerBenchmark, SearchSingleSizeTFieldNoMatches)(benchmark::State& state) {
+    Query query = Query::create("zip_code", QueryType::EQUALS, std::numeric_limits<size_t>::max());
+
+    for (auto _ : state) {
+        std::vector<const Collision*> results = collision_manager->search(query);
+        benchmark::DoNotOptimize(results);
+    }
+}
+
+BENCHMARK_DEFINE_F(CollisionManagerBenchmark, SearchSingleSizeTFieldSomeMatches)(benchmark::State& state) {
+    Query query = Query::create("zip_code", QueryType::EQUALS, 11208ULL);
+
+    for (auto _ : state) {
+        std::vector<const Collision*> results = collision_manager->search(query);
+        benchmark::DoNotOptimize(results);
+    }
+}
+
+BENCHMARK_REGISTER_F(CollisionManagerBenchmark, SearchSingleStringFieldNoMatches)->Iterations(50);
+BENCHMARK_REGISTER_F(CollisionManagerBenchmark, SearchSingleStringFieldSomeMatches)->Iterations(50);
+BENCHMARK_REGISTER_F(CollisionManagerBenchmark, SearchSingleSizeTFieldNoMatches)->Iterations(50);
+BENCHMARK_REGISTER_F(CollisionManagerBenchmark, SearchSingleSizeTFieldSomeMatches)->Iterations(50);
+
+BENCHMARK_MAIN();
