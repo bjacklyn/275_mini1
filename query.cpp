afce3a309bddebd01b01e3c5d777aea1afa0a96c
Brandon Jacklyn <brandonjacklyn@live.ca>
2025-02-12 14:17:35 -0800

    Throw invalid_argument exception instead of using asserts for invalid queries

--- query.cpp
+++ query.cpp
@@ -1,7 +1,7 @@
 #include "query.hpp"
 
-#include <cassert>
 #include <chrono>
+#include <stdexcept>
 #include <string>
 #include <string_view>
 
@@ -34,25 +34,35 @@ FieldQuery Query::create_field_query(const std::string_view& name,
         using T = std::decay_t<decltype(val)>;
 
         if constexpr (std::is_same_v<T, float>) {
-            assert((name == "latitude" || name == "longitude") && "Invalid field_name provided!");
+            if (name != "latitude" && name != "longitude") {
+                throw std::invalid_argument("Invalid field_name provided for float!");
+            }
         } else if constexpr (std::is_same_v<T, std::size_t>) {
-            assert((name == "zip_code" || name == "number_of_persons_injured" ||
-                name == "number_of_persons_killed" || name == "number_of_pedestrians_injured" ||
-                name == "number_of_pedestrians_killed" || name == "number_of_cyclist_injured" ||
-                name == "number_of_cyclist_killed" || name == "number_of_motorist_injured" ||
-                name == "number_of_motorist_killed" || name == "collision_id") && "Invalid field_name provided!");
+            if (name != "zip_code" && name != "number_of_persons_injured" &&
+                name != "number_of_persons_killed" && name != "number_of_pedestrians_injured" &&
+                name != "number_of_pedestrians_killed" && name != "number_of_cyclist_injured" &&
+                name != "number_of_cyclist_killed" && name != "number_of_motorist_injured" &&
+                name != "number_of_motorist_killed" && name != "collision_id") {
+                throw std::invalid_argument("Invalid field_name provided for std::size_t!");
+            }
         } else if constexpr (std::is_same_v<T, std::string>) {
-            assert((name == "borough" || name == "location" || name == "on_street_name" ||
-                name == "cross_street_name" || name == "off_street_name" ||
-                name == "contributing_factor_vehicle_1" || name == "contributing_factor_vehicle_2" ||
-                name == "contributing_factor_vehicle_3" || name == "contributing_factor_vehicle_4" ||
-                name == "contributing_factor_vehicle_5" || name == "vehicle_type_code_1" ||
-                name == "vehicle_type_code_2" || name == "vehicle_type_code_3" ||
-                name == "vehicle_type_code_4" || name == "vehicle_type_code_5") && "Invalid field_name provided!");
+            if (name != "borough" && name != "location" && name != "on_street_name" &&
+                name != "cross_street_name" && name != "off_street_name" &&
+                name != "contributing_factor_vehicle_1" && name != "contributing_factor_vehicle_2" &&
+                name != "contributing_factor_vehicle_3" && name != "contributing_factor_vehicle_4" &&
+                name != "contributing_factor_vehicle_5" && name != "vehicle_type_code_1" &&
+                name != "vehicle_type_code_2" && name != "vehicle_type_code_3" &&
+                name != "vehicle_type_code_4" && name != "vehicle_type_code_5") {
+                throw std::invalid_argument("Invalid field_name provided for std::string!");
+            }
         } else if constexpr (std::is_same_v<T, std::chrono::year_month_day>) {
-            assert((name == "crash_date") && "Invalid field_name provided!");
+            if (name != "crash_date") {
+                throw std::invalid_argument("Invalid field_name provided for std::chrono::year_month_day!");
+            }
         } else if constexpr (std::is_same_v<T, std::chrono::hh_mm_ss<std::chrono::minutes>>) {
-            assert((name == "crash_time") && "Invalid field_name provided!");
+            if (name != "crash_time") {
+                throw std::invalid_argument("Invalid field_name provided for std::chrono::hh_mm_ss!");
+            }
         }
     }, value);
 
