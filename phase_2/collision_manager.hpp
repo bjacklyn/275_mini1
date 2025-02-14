3cc10b6351e653919f79f187edbe38c4d8025b2c
Brandon Jacklyn <brandonjacklyn@live.ca>
2025-02-14 11:06:06 -0800

    Create separate directories for each phase of work

---
+++ phase_2/collision_manager.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "collision.hpp"
+#include "query.hpp"
+
+#include <string>
+
+
+class CollisionManager {
+
+public:
+    CollisionManager(const std::string& filename);
+
+    bool is_initialized();
+    const std::string& get_initialization_error();
+    const std::vector<const Collision*> search(const Query& query);
+
+    friend class CollisionManagerTest;
+
+private:
+    CollisionManager(std::vector<Collision>& collisions);
+
+    std::string initialization_error_;
+    std::vector<Collision> collisions_;
+
+};
