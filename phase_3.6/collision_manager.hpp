cb7117dc383bfae59f3657536d565ce022d460de
Brandon Jacklyn <brandonjacklyn@live.ca>
2025-02-16 19:59:10 -0800

    Create phase 3.6 from phase 3.5

---
+++ phase_3.6/collision_manager.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "collision.hpp"
+#include "query.hpp"
+
+#include <string>
+
+
+class CollisionManager {
+
+public:
+    CollisionManager(const std::string& filename);
+
+    bool is_initialized();
+    const std::string& get_initialization_error();
+    const std::vector<CollisionProxy*> searchOpenMp(const Query& query);
+
+    friend class CollisionManagerTest;
+
+private:
+    CollisionManager(Collisions& collisions);
+    CollisionManager(const std::vector<Collision>& collisions);
+
+    const CollisionProxy index_to_collision(const std::size_t index);
+    void create_proxies();
+
+    std::string initialization_error_;
+    Collisions collisions_;
+    std::vector<CollisionProxy> collision_proxies_;
+
+};
